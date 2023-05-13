4350ce1ef6209bcfb4d4e1f00667f11f23d1f996
Christian Amauger <christian.amauger@dti.ulaval.ca>
2023-05-13 09:39:03 -0400

    fix freeze problem with image urls

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import json
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 
 def get_image_url(filename):
     base_url = 'https://zippy-rabanadas-30839c.netlify.app'
     return f'{base_url}/static/{filename}'
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', static_url_path='/static')
 
 def get_character_data(id):
     with open('characters_with_images.json', 'r') as f:
@@ -17,34 +17,23 @@ def get_character_data(id):
 
 def load_characters():
     with open('characters_with_images.json', 'r') as f:
-        characters = json.load(f)   
+        characters = json.load(f)
     return characters
 
 # Route for the home page
 @app.route('/')
 def index():
-    characters = load_characters()
-    processed_characters = []
-
-    for character in characters:
-        processed_characters.append({
-            'id': character.get('id'),
-            'full_name': character.get('first_name') + ' ' + character.get('last_name'),
-            'image_url': f"{character.get('id')}.png"
-            # Include other character attributes as needed
-        })
-
-    return render_template('index.html', characters=processed_characters)
-
+    # Render the template
+    return render_template('index.html', characters=load_characters())
 
 # Route for individual character based on ID
 @app.route('/character/<int:id>.html')
 def character(id):
     # Retrieve character data based on the ID
     character_data = get_character_data(id)
-    
+
     # Process character data and render the template
-    return render_template('character.html', character=character_data, static_url_path='/static')
+    return render_template('character.html', character=character_data)
 
 if __name__ == '__main__':
     app.run(debug=False)
