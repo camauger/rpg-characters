d838c8a411869e51efe5c026a3c09db25d18ee98
Christian Amauger <christian.amauger@dti.ulaval.ca>
2023-05-13 10:21:54 -0400

    fix freeze problem with image urls

--- freeze.py
+++ freeze.py
@@ -1,12 +1,15 @@
+import os
 from flask_frozen import Freezer
 import json
 from app import app
 
 freezer = Freezer(app)
+
 def configure_app(app):
     app.config['FREEZER_RELATIVE_URLS'] = True
-    app.config['FREEZER_BASE_URL'] = 'https://zippy-rabanadas-30839c.netlify.app'
-
+    app.config['FREEZER_BASE_URL'] = os.environ.get('FREEZER_BASE_URL', '')
+    # app.config['FREEZER_BASE_URL'] = 'https://zippy-rabanadas-30839c.netlify.app'
+freezer = Freezer(app)
 
 @freezer.register_generator
 def character():
@@ -18,3 +21,5 @@ def character():
 if __name__ == '__main__':
     configure_app(app)
     freezer.freeze()
+
+
