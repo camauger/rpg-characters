df8a65b79755b1f02f7f38339ee4431407b42dd2
Christian Amauger <christian.amauger@gmail.com>
2023-05-24 13:32:50 -0400

    picture_id creation for characters

--- models/image_prompt_class.py
+++ models/image_prompt_class.py
@@ -34,7 +34,7 @@ class ImagePrompt:
 
     def tags(self):
         keywords = ', '.join(self.character.ethnicity_keywords)
-        return f"{keywords}, Forgotten Realms, Medieval Fantasy, D&D, Dungeons & Dragons. --s 1000 --upbeta --seed {self.character.id}"
+        return f"{keywords}, Fantasy, Medieval Fantasy, Dungeons & Dragons. --s 1000 --upbeta --seed {self.character.picture_id}"
 
     def actor(self):
         return f"{self.character.create_physical_description_text()}"
