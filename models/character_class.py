f07562d4ba08b39594f88c1e3d7c4367b2162c5c
Christian Amauger <christian.amauger@gmail.com>
2023-05-24 13:43:04 -0400

    adding error handling in Character class

--- models/character_class.py
+++ models/character_class.py
@@ -7,8 +7,8 @@ import requests
 import json
 from utils.indefinite_article import indefinite_article
 from api_settings import api_key
-# Create a background story for an RPG character
 
+# Create a background story for an RPG character
 def fetch_character_data(prompt, api_key):
     url = "https://api.openai.com/v1/engines/text-davinci-002/completions"
     headers = {
@@ -22,13 +22,22 @@ def fetch_character_data(prompt, api_key):
         "stop": None,
         "temperature": 0.7,
     }
-    response = requests.post(url, headers=headers, data=json.dumps(data))
-    response_json = response.json()
 
-    if response.status_code != 200:
-        raise Exception(f"Failed to fetch data from ChatGPT: {response_json}")
+    try:
+        response = requests.post(url, headers=headers, data=json.dumps(data))
+        response.raise_for_status()
+        response_json = response.json()
 
-    return response_json['choices'][0]['text']
+        if response.status_code == 200:
+            return response_json['choices'][0]['text']
+        else:
+            raise Exception(f"Failed to fetch data from ChatGPT: {response_json}")
+
+    except requests.exceptions.RequestException as e:
+        raise Exception(f"Error occurred during API request: {e}")
+
+    except (KeyError, IndexError) as e:
+        raise Exception(f"Invalid response received from ChatGPT: {e}")
 
 """
 This code is defining a class called Character.
@@ -42,40 +51,61 @@ Additionally, it creates physical and psychological descriptions for the charact
 Finally, the __str__ method is used to return a string representation of the character.
 """
 
-
 class Character:
-    def __init__(self, id, params):
-        self.id = id
-        self.first_name = generate_random_first_name(params['gender'])
-        self.last_name = generate_random_last_name()
-        self.full_name = f"{self.first_name} {self.last_name}"
-        self.gender = params['gender']
-        self.character_class = params['random_class'].get('name')
-        self.character_class_id = params['random_class'].get('id')
-        self.character_subclass = params['random_subclass'].get('name')
-        self.character_subclass_id = params['random_subclass'].get('id')
-        self.background = params['background']
-        self.background_name = params['background']['name']
-        self.background_id = params['background']['id']
-        self.ethnicity = params['random_ethnicity']['race']
-        self.ethnicity_name = params['random_ethnicity']['race']
-        self.ethnicity_id = params['random_ethnicity']['id']
-        self.subrace = params['random_subrace']
-        self.subrace_name = self.get_subrace_name()
-        self.subrace_id = params['random_subrace']['id']
-        self.ethnicity_keywords = get_ethnicity_keywords(params['random_ethnicity'], params['random_subrace'])
-        self.age = params['age']
-        self.physical_description = self.create_physical_description()
-        self.physical_trait = create_physical_trait()
-        self.hair_color = create_hair_color()
-        self.hair_style = create_hair_style()
-        self.eye_color = create_eye_color()
-        self.behavior = self.create_behavior()
-        self.image_prompt = self.create_image_prompt()
-        # Has to be last!
-        self.personality_description = self.create_personality_description()
-        self.background_story = self.create_background_story()
-        self.picture_id = self.create_full_id()
+    def __init__(self, character_id, params):
+        # Initialize the Character object with the provided ID and parameters
+        try:
+            # Assign basic character information
+            self.id = character_id
+            self.first_name = generate_random_first_name(params['gender'])
+            self.last_name = generate_random_last_name()
+            self.full_name = f"{self.first_name} {self.last_name}"
+            self.gender = params['gender']
+        
+            # Assign character class information
+            random_class = params.get('random_class', {})
+            self.character_class = random_class.get('name')
+            self.character_class_id = random_class.get('id')
+
+            # Assign character subclass information
+            random_subclass = params.get('random_subclass', {})
+            self.character_subclass = random_subclass.get('name')
+            self.character_subclass_id = random_subclass.get('id')
+        
+            # Assign background information
+            self.background = params.get('background', {})
+            self.background_name = self.background.get('name')
+            self.background_id = self.background.get('id')
+
+            # Assign ethnicity information
+            random_ethnicity = params.get('random_ethnicity', {})
+            self.ethnicity = random_ethnicity.get('race')
+            self.ethnicity_name = random_ethnicity.get('race')
+            self.ethnicity_id = random_ethnicity.get('id')
+
+            # Assign subrace information
+            self.subrace = params.get('random_subrace', {})
+            self.subrace_name = self.subrace.get('name')
+            self.subrace_id = self.subrace.get('id')
+        
+            # Assign ethnicity keywords
+            self.ethnicity_keywords = get_ethnicity_keywords(random_ethnicity, self.subrace)
+            
+            # Assign other character attributes
+            self.age = params.get('age')
+            self.physical_description = self.create_physical_description()
+            self.physical_trait = create_physical_trait()
+            self.hair_color = create_hair_color()
+            self.hair_style = create_hair_style()
+            self.eye_color = create_eye_color()
+            self.behavior = self.create_behavior()
+            self.image_prompt = self.create_image_prompt()
+            self.personality_description = self.create_personality_description()
+            self.background_story = self.create_background_story()
+            self.picture_id = self.create_picture_id()
+        
+        except (KeyError, TypeError) as e:
+            raise Exception(f"Invalid parameter format: {e}")
 
     def get_subrace_name(self):
         if self.subrace is not None:
